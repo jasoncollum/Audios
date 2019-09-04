c59e6df87fe6554a6eb23a4ab6459636e817a18a
unknown <jasoncollum@gmail.com>
2019-09-04 09:33:47 -0700

    setup Vocal Model

--- Audios/Models/Vocal.cs
+++ Audios/Models/Vocal.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,10 @@ namespace Audios.Models
 {
     public class Vocal
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Vocal")]
+        public string Type { get; set; }
     }
 }
