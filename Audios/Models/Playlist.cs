79fcfe3ae34ce88dcf45d56f19af0c3c79087195
unknown <jasoncollum@gmail.com>
2019-09-04 09:39:35 -0700

    setup Playlist Model

--- Audios/Models/Playlist.cs
+++ Audios/Models/Playlist.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,11 @@ namespace Audios.Models
 {
     public class Playlist
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Playlist Name")]
+        public string Name { get; set; }
     }
 }
