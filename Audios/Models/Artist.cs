087f064237c97bf9e50590d19a3df216c46624b6
unknown <jasoncollum@gmail.com>
2019-09-04 09:06:34 -0700

    setup Artist Model

--- Audios/Models/Artist.cs
+++ Audios/Models/Artist.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,13 @@ namespace Audios.Models
 {
     public class Artist
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Artist")]
+        public string Name { get; set; }
+
+        public string ImagePath { get; set; }
     }
 }
