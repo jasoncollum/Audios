00176ebb43c9ae4fb7754906474549dfd2ee2da1
unknown <jasoncollum@gmail.com>
2019-09-04 09:28:08 -0700

    setup Publisher Model

--- Audios/Models/Publisher.cs
+++ Audios/Models/Publisher.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,10 @@ namespace Audios.Models
 {
     public class Publisher
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Publisher")]
+        public string Name { get; set; }
     }
 }
