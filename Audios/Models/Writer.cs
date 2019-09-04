77a1fe40fd99ed38cf85ef8dc6ec56c2ecbf7b70
unknown <jasoncollum@gmail.com>
2019-09-04 09:25:15 -0700

    setup Writer Model

--- Audios/Models/Writer.cs
+++ Audios/Models/Writer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,19 @@ namespace Audios.Models
 {
     public class Writer
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Writer")]
+        public string Name { get; set; }
+
+        public int PublisherId { get; set; }
+
+        public Publisher Publisher { get; set; }
+
+        public int PROId { get; set; }
+
+        public PRO PRO { get; set; }
     }
 }
