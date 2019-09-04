491d19aa15de122de59e576dda81f9dcca275378
unknown <jasoncollum@gmail.com>
2019-09-04 08:01:30 -0700

    first commit

---
+++ Audios/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Audios.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
