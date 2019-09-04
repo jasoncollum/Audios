d39d9239676d093875817696fbfbeeb14f113a11
unknown <jasoncollum@gmail.com>
2019-09-04 09:19:27 -0700

    setup Song model

--- Audios/Models/Song.cs
+++ Audios/Models/Song.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,31 @@ namespace Audios.Models
 {
     public class Song
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public decimal? Bpm { get; set; }
+
+        [Display(Name = "Search Words")]
+        public string SearchWords { get; set; }
+
+        public string Lyrics { get; set; }
+
+        [Display(Name = "One Stop")]
+        public bool isOneStop { get; set; }
+
+        public string AudioPath { get; set; }
+
+        [Required]
+        [Display(Name = "Vocal")]
+        public int VocalId { get; set; }
+
+        [Required]
+        public int ArtistId { get; set; }
+
+        public Artist Artist { get; set; }
     }
 }
