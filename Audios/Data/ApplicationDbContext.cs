3d16fec79e37007d071382d64211ff4b3d1e2751
unknown <jasoncollum@gmail.com>
2019-09-05 07:43:09 -0700

    setup ApplicationUser Model

--- Audios/Data/ApplicationDbContext.cs
+++ Audios/Data/ApplicationDbContext.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Audios.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Audios.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     }
 }
---
+++ Audios/Models/ApplicationUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace Audios.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Name")]
+        public string FullName => $"{FirstName} {LastName}";
+
+        public virtual ICollection<Playlist> Playlists { get; set; }
+    }
+}
